65c25610543151af76e05916c879376d7dff568d
Zhihong Ye <yezhihong@gmail.com>
2015-04-02 11:00:30 -0400

    add VGG code dvcsjlab

--- VGG/env_ld.sh
+++ VGG/env_ld.sh
@@ -1,13 +1,5 @@
 #!/bin/csh
-#source /site/env/syslogin
-#source /site/env/sysapps
-#setenv CVSROOT /group/halla/analysis/cvs
-setenv LD_LIBRARY_PATH /work/halla/dvcs/disk1/mongi/envr:${LD_LIBRARY_PATH}
-#source /u/apps/root/PRO/setroot_CUE
-#setenv  /u/site/12gev_phys/ce/
-#setenv 
-#source /u/home/mongi/.login
+setenv WORKDIR /work/halla/solid/yez/dvcs/VGG
+setenv LD_LIBRARY_PATH ${WORKDIR}/envr:${LD_LIBRARY_PATH}
 
-  /w/halla-scifs2/dvcs/mongi/dvcs/VGG/dvcsjlab 
-# /w/halla/dvcs/mongi/dvcs/VGG/ls > file.out
-# /w/halla/dvcs/mongi/dvcs/VGG/whoami > file2.out
+${WORKDIR}/dvcsjlab 
